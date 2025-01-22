058bb69fd76951165ff53cf194ca8bfd9b8bb6d2
Lukas Gabriel <lukasgabriel@lukasgabriel.net>
2025-01-23 00:54:44 +0100

    Move resources to subfolders

--- characters/cat/camera_2d.gd
+++ characters/cat/camera_2d.gd
@@ -1,7 +1,9 @@
 extends Camera2D
 
+
 var screen_size: Vector2
 var level_height: int = 2
+
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -9,6 +11,7 @@ func _ready() -> void:
 	
 	limit_bottom = screen_size.x * 1.5
 	limit_top = -screen_size.y * (level_height - 1)
+
 
 func _process(delta: float) -> void:
 	pass
